a058bdcfe7bd46995bc17998b3add04872858163
xiaoxiaoyu <383108440@qq.com>
2015-09-22 16:39:28 +0800

    add user element

---
+++ app/src/main/java/org/phphub/app/api/entity/element/User.java
@@ -0,0 +1,34 @@
+package org.phphub.app.api.entity.element;
+
+public class User {
+
+    int id;
+
+    String name;
+
+    String avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
