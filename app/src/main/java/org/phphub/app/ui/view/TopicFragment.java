17f46b0f6e4ad874aa401b3115f4716f97e44562
NauxLiu <nauxliu@gmail.com>
2015-09-24 00:10:53 +0800

    improve ui

--- app/src/main/java/org/phphub/app/ui/view/TopicFragment.java
+++ app/src/main/java/org/phphub/app/ui/view/TopicFragment.java
@@ -54,7 +54,7 @@ public class TopicFragment extends BaseSupportFragment<TopicPresenter> implement
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.topic, container, false);
+        return inflater.inflate(R.layout.topic_list, container, false);
     }
 
     @Override
