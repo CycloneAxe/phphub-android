f51fb7a77ec26915cf3acb7a327d2f9693afab64
xiaoxiaoyu <383108440@qq.com>
2015-09-29 18:05:46 +0800

    add node type

---
+++ app/src/main/java/org/phphub/app/api/entity/NodeEntity.java
@@ -0,0 +1,33 @@
+package org.phphub.app.api.entity;
+
+import org.phphub.app.api.entity.element.Node;
+
+import java.util.List;
+
+public class NodeEntity {
+
+    public class NodeList {
+        public List<Node> data;
+
+        public List<Node> getData() {
+            return data;
+        }
+
+        public void setData(List<Node> data) {
+            this.data = data;
+        }
+    }
+
+    public class UserObj {
+        public Node data;
+
+        public Node getData() {
+            return data;
+        }
+
+        public void setData(Node data) {
+            this.data = data;
+        }
+    }
+
+}
---
+++ app/src/main/java/org/phphub/app/api/entity/element/Node.java
@@ -0,0 +1,24 @@
+package org.phphub.app.api.entity.element;
+
+public class Node {
+
+    int id;
+
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
