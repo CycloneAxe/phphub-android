98cebf00a7630c55300aaefca2fc18507246aa21
xiaoxiaoyu <383108440@qq.com>
2015-10-10 10:36:33 +0800

    add topic presenter

--- app/src/main/java/org/phphub/app/common/internal/di/component/ApiComponent.java
+++ app/src/main/java/org/phphub/app/common/internal/di/component/ApiComponent.java
@@ -3,6 +3,7 @@ package org.phphub.app.common.internal.di.component;
 import org.phphub.app.common.internal.di.module.ApiModule;
 import org.phphub.app.ui.presenter.MainPresenter;
 import org.phphub.app.ui.presenter.RecommendedPresenter;
+import org.phphub.app.ui.presenter.TopicDetailPresenter;
 import org.phphub.app.ui.presenter.TopicPresenter;
 import org.phphub.app.ui.presenter.WikiPresenter;
 
@@ -20,4 +21,6 @@ public interface ApiComponent {
         void inject(RecommendedPresenter recommendedPresenter);
 
         void inject(WikiPresenter wikiPresenter);
+
+        void inject(TopicDetailPresenter topicDetailPresenter);
 }
