10df0a138f135a6af54ebd3903fb092f00905a9c
xiaoxiaoyu <383108440@qq.com>
2015-10-13 17:07:54 +0800

    change link data

--- app/src/main/java/org/phphub/app/api/entity/element/Link.java
+++ app/src/main/java/org/phphub/app/api/entity/element/Link.java
@@ -6,11 +6,22 @@ public class Link {
     @SerializedName("details_web_view")
     protected String detailsWebView;
 
+    @SerializedName("replies_web_view")
+    protected String repliesWebView;
+
     public String getDetailsWebView() {
         return detailsWebView;
     }
 
     public void setDetailsWebView(String detailsWebView) {
         this.detailsWebView = detailsWebView;
+    }
+
+    public String getRepliesWebView() {
+        return repliesWebView;
+    }
+
+    public void setRepliesWebView(String repliesWebView) {
+        this.repliesWebView = repliesWebView;
     }
 }
