47702e3b19de55e43470af88cb20f8bf87a9cf7f
xiaoxiaoyu <383108440@qq.com>
2015-10-14 15:07:34 +0800

    add vote dialog

--- app/src/main/java/org/phphub/app/widget/AlertDialog.java
+++ app/src/main/java/org/phphub/app/widget/AlertDialog.java
@@ -2,12 +2,17 @@ package org.phphub.app.widget;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 
 import org.phphub.app.R;
 
 public class AlertDialog extends Dialog {
+    float widthProportion;
+    float heightProportion;
+
     private Window window = null;
 
     public AlertDialog(Context context) {
@@ -22,12 +27,20 @@ public class AlertDialog extends Dialog {
         super(context, cancelable, cancelListener);
     }
 
-    public void popupDialog(int layoutResID) {
+    public void popupDialog(int layoutResID, float wp, float hp, boolean noTitle) {
+        if (noTitle) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+        }
+
         setContentView(layoutResID);
+
+        this.widthProportion = wp;
+        this.heightProportion = hp;
 
         windowDeploy();
 
         setCanceledOnTouchOutside(true);
+
         show();
     }
 
@@ -35,13 +48,24 @@ public class AlertDialog extends Dialog {
         window = getWindow();
 
         window.setWindowAnimations(R.style.dialogWindow);
-        window.setBackgroundDrawableResource(R.color.white); //设置对话框背景为透明
+        window.setBackgroundDrawableResource(R.drawable.bg_dialog); //设置对话框背景为透明
         WindowManager.LayoutParams wl = window.getAttributes();
         //根据x，y坐标设置窗口需要显示的位置
         wl.x = 0; //x小于0左移，大于0右移
         wl.y = 0; //y小于0上移，大于0下移
 //            wl.alpha = 0.6f; //设置透明度
 //            wl.gravity = Gravity.BOTTOM; //设置重力
+
         window.setAttributes(wl);
+
+        WindowManager m = window.getWindowManager();
+        Display d = m.getDefaultDisplay();
+
+        WindowManager.LayoutParams params = window.getAttributes();
+
+        params.width = (int) (d.getWidth() * this.widthProportion);
+        params.height = (int) (d.getHeight() * this.heightProportion);
+
+        window.setAttributes(params);
     }
 }
