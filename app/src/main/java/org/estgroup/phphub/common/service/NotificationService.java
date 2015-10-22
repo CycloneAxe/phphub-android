b17d668c8a28501a04bc2b0fb106a7d3b313bf34
xiaoxiaoyu <383108440@qq.com>
2015-10-22 16:19:05 +0800

    add if logged

--- app/src/main/java/org/estgroup/phphub/common/service/NotificationService.java
+++ app/src/main/java/org/estgroup/phphub/common/service/NotificationService.java
@@ -1,26 +1,16 @@
 package org.estgroup.phphub.common.service;
 
-import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
 
-import com.orhanobut.logger.Logger;
-import com.squareup.otto.Produce;
-
-import org.estgroup.phphub.R;
 import org.estgroup.phphub.api.entity.NotificationEntity;
 import org.estgroup.phphub.api.entity.element.Notification;
-import org.estgroup.phphub.common.event.NotificationChangeEvent;
-import org.estgroup.phphub.common.provider.BusProvider;
+import org.estgroup.phphub.common.App;
 import org.estgroup.phphub.common.util.Utils;
 import org.estgroup.phphub.model.UserModel;
-
-import eu.unicate.retroauth.AuthAccountManager;
-import rx.functions.Func1;
 
 import java.util.List;
 import java.util.Timer;
@@ -29,33 +19,48 @@ import java.util.TimerTask;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 import static org.estgroup.phphub.common.qualifier.AuthType.AUTH_TYPE_USER;
 
 public class NotificationService extends Service {
-
-    private AccountManager accountManager;
-
-    private String accountType, tokenType;
-
-    private Account[] accounts;
 
     public static int UPDATE_INTERVAL = 3000;
     private Timer timer = new Timer();
 
     @Inject
+    AccountManager accountManager;
+
+    @Inject
     @Named(AUTH_TYPE_USER)
     UserModel userModel;
 
-    @Inject
-    AuthAccountManager authAccountManager;
+    Subscriber subscriber;
+
+    NotificationListener listener;
+
+    public static interface NotificationListener {
+        void onNotificationServiceSuccess(List<Notification> notificationList);
+
+        void onNotificationServiceError(RetrofitError error);
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        System.out.println("onCreate");
+        ((App) getApplication()).getApiComponent().inject(this);
     }
 
+    public void setListener(NotificationListener listener) {
+        this.listener = listener;
+    }
 
     @Override
     public void onDestroy() {
@@ -68,15 +73,6 @@ public class NotificationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        System.out.println("onStartCommand");
-        if (Utils.logined(this, AccountManager.get(this))) {
-            this.accountManager = AccountManager.get(this);
-            this.accountType = getString(R.string.auth_account_type);
-            this.tokenType = getString(R.string.auth_token_type);
-            this.accounts = accountManager.getAccountsByType(accountType);
-
-            getNotification();
-        }
 
         return START_STICKY;
     }
@@ -85,7 +81,21 @@ public class NotificationService extends Service {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                System.out.println("MyService");
+                if (Utils.logined(NotificationService.this, accountManager)) {
+                    userModel.getMyNotifications()
+                            .map(new Func1<NotificationEntity, List<Notification>>() {
+                                @Override
+                                public List<Notification> call(NotificationEntity notificationEntity) {
+                                    return notificationEntity.getData();
+                                }
+                            })
+                            .subscribe(new Action1<List<Notification>>() {
+                                @Override
+                                public void call(List<Notification> notificationList) {
+                                    listener.onNotificationServiceSuccess(notificationList);
+                                }
+                            });
+                }
             }
         }, 0, UPDATE_INTERVAL);
     }
