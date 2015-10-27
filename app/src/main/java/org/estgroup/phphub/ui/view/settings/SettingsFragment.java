e2d87970952b55c0d30a5073e18e3172cb78bbe4
xiaoxiaoyu <383108440@qq.com>
2015-10-27 15:54:47 +0800

    add feedback

--- app/src/main/java/org/estgroup/phphub/ui/view/settings/SettingsFragment.java
+++ app/src/main/java/org/estgroup/phphub/ui/view/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package org.estgroup.phphub.ui.view.settings;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -7,11 +8,23 @@ import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AlertDialog;
 
+import com.umeng.fb.FeedbackAgent;
+import com.umeng.fb.model.UserInfo;
+import com.umeng.message.PushAgent;
+
 import org.estgroup.phphub.R;
 import org.estgroup.phphub.common.App;
 import org.estgroup.phphub.common.util.Utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
+
+import eu.unicate.retroauth.AuthAccountManager;
+
+import static org.estgroup.phphub.common.Constant.USERNAME_KEY;
+import static org.estgroup.phphub.common.Constant.USER_ID_KEY;
 
 public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
 
@@ -20,12 +33,19 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
     @Inject
     AccountManager accountManager;
 
+    @Inject
+    AuthAccountManager authAccountManager;
+
+    Account account;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-
         ((App) getActivity().getApplication()).getAppComponent().inject(this);
+        account = Utils.getActiveAccount(getActivity(), authAccountManager);
+
+        ((Preference) findPreference("feedback")).setOnPreferenceClickListener(this);
     }
 
     @Override
@@ -43,6 +63,42 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
     @Override
     public boolean onPreferenceClick(Preference preference) {
         switch (preference.getKey()) {
+            case "feedback":
+                final FeedbackAgent agent = new FeedbackAgent(getActivity());
+                agent.startFeedbackActivity();
+
+                com.umeng.fb.model.UserInfo info = agent.getUserInfo();
+                if (info == null) {
+                    info = new UserInfo();
+                }
+
+                Map<String, String> contact = info.getContact();
+                if (contact == null) {
+                    contact = new HashMap<String, String>();
+                }
+
+                if (Utils.logined(getActivity(), accountManager)) {
+                    contact.put("plain", "uid: "+ accountManager.getUserData(account, USER_ID_KEY) + " uname: "+accountManager.getUserData(account, USERNAME_KEY));
+
+                    info.setContact(contact);
+
+                    agent.setUserInfo(info);
+                }
+
+                agent.sync();
+                agent.openAudioFeedback();
+                agent.openFeedbackPush();
+                PushAgent.getInstance(getActivity()).setDebugMode(true);
+                PushAgent.getInstance(getActivity()).enable();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean result = agent.updateUserInfo();
+                    }
+                }).start();
+                return true;
+
             case LOGOUT_KEY:
                 new AlertDialog.Builder(getActivity())
                         .setMessage("确认退出吗？")
