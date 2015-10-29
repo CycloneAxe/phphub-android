2dace6f79992e6bff28ec9c8f843739695c7b23f
CycloneAxe <matrixlity@gmail.com>
2015-10-29 11:37:29 +0800

    add GalleryActivity

--- app/src/main/java/org/estgroup/phphub/ui/view/GalleryActivity.java
+++ app/src/main/java/org/estgroup/phphub/ui/view/GalleryActivity.java
@@ -2,12 +2,50 @@ package org.estgroup.phphub.ui.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.imagepipeline.image.ImageInfo;
 
 import org.estgroup.phphub.R;
 import org.estgroup.phphub.common.base.BaseActivity;
 
+import butterknife.Bind;
+import me.relex.photodraweeview.PhotoDraweeView;
+
 public class GalleryActivity extends BaseActivity {
     private final static String IMAGE_URL = "image_url";
+
+    @Bind(R.id.zoom_photo)
+    PhotoDraweeView photoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String imageUrl = getIntent().getStringExtra(IMAGE_URL);
+        if (!TextUtils.isEmpty(imageUrl)) {
+            PipelineDraweeControllerBuilder builder = Fresco.newDraweeControllerBuilder();
+            builder.setUri(Uri.parse(imageUrl));
+            builder.setOldController(photoView.getController());
+            builder.setControllerListener(new BaseControllerListener<ImageInfo>() {
+                @Override
+                public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
+                    super.onFinalImageSet(id, imageInfo, animatable);
+                    if (imageInfo == null || photoView == null) {
+                        return;
+                    }
+                    photoView.update(imageInfo.getWidth(), imageInfo.getHeight());
+                }
+            });
+            photoView.setController(builder.build());
+        }
+    }
 
     @Override
     protected int getLayoutResId() {
